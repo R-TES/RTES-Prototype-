fed3e1ba4743b35106b0b1813b3696199e1e83bf
Corruption13 <sandeep.pillai42@gmail.com>
2022-03-10 04:02:23 +0530

    Psychosis

--- webdemo/Build/webdemo.framework.js
+++ webdemo/Build/webdemo.framework.js
@@ -1303,7 +1303,7 @@ function _emscripten_asm_const_id(code, a0) {
  return ASM_CONSTS[code](a0);
 }
 STATIC_BASE = GLOBAL_BASE;
-STATICTOP = STATIC_BASE + 3002320;
+STATICTOP = STATIC_BASE + 3006e3;
 __ATINIT__.push({
  func: (function() {
   __GLOBAL__sub_I_AccessibilityScriptingClasses_cpp();
@@ -3369,7 +3369,7 @@ __ATINIT__.push({
   ___emscripten_environ_constructor();
  })
 });
-var STATIC_BUMP = 3002320;
+var STATIC_BUMP = 3006e3;
 Module["STATIC_BASE"] = STATIC_BASE;
 Module["STATIC_BUMP"] = STATIC_BUMP;
 var tempDoublePtr = STATICTOP;
