5cdf00268d0684e361a8b79fb18ba05c1501ca8d
Corruption13 <sandeey999@gmail.com>
2022-05-05 20:12:35 +0530

    fixed proximity bug!

--- Assets/Assets/Scripts/Proximity Audio/ProximityAudioController.cs
+++ Assets/Assets/Scripts/Proximity Audio/ProximityAudioController.cs
@@ -5,54 +5,50 @@ using Photon.Pun;
 using Scripts; // Agora
 
 
-    public class ProximityAudioController : MonoBehaviour
+public class ProximityAudioController : MonoBehaviour
+{
+    private string[] blockedUsers;
+    public GameObject player; 
+
+
+    void Start()
     {
-        private bool allowingSubscribers = true;
-        private string[] blockedUsers; 
-
-        private void Start()
-        {
-            BindEntityaWithLocalPlayer();   
-            // Bind as child the Script object onto the player object controlled by current user.
-        }
-
-        private void OnTriggerEnter2D(Collider2D collision)
-        {
-            if (collision.gameObject.CompareTag("Player"))
-            {
-                string playerID = collision.gameObject.GetComponent<PhotonView>().Owner.NickName;
-                if (playerID == PhotonNetwork.LocalPlayer.NickName) return; // Probably can remove this. Too lazy to check if it'll break.
-
-                Debug.Log("UNITY DEBUG LOG:\n A user has ENTERED your proximity: " + playerID);
-                if (playerID != null)
-                {
-                    Agora.Subscribe(playerID);
-                }
-            }
-        }
-
-        private void OnTriggerExit2D(Collider2D collision)
-        {
-            if (collision.gameObject.CompareTag("Player"))
-            {
-                string playerID = collision.gameObject.GetComponent<PhotonView>().Owner.NickName;
-                if (playerID == PhotonNetwork.LocalPlayer.NickName) return;
-
-                Debug.Log("UNITY DEBUG LOG:\n A user has EXITED your proximity: " + playerID);
-                if (playerID != null)
-                {
-                    Agora.Unsubscribe(playerID);
-                }
-            }
-        }
-
-        void BindEntityaWithLocalPlayer()
-        {
-            GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
-            gameObject.transform.parent = players[0].transform;
-            gameObject.transform.position = gameObject.transform.parent.position;
-        }
-
+        if (!player.GetComponent<PhotonView>().IsMine)
+            Destroy(gameObject);
+      
     }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            string playerID = collision.gameObject.GetComponent<PhotonView>().Owner.NickName;
+            if (playerID == PhotonNetwork.LocalPlayer.NickName) return; // Probably can remove this. Too lazy to check if it'll break.
+
+            Debug.Log("UNITY DEBUG LOG:\n A user has ENTERED your proximity: " + playerID);
+            if (playerID != null)
+            {
+                Agora.Subscribe(playerID);
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            string playerID = collision.gameObject.GetComponent<PhotonView>().Owner.NickName;
+            if (playerID == PhotonNetwork.LocalPlayer.NickName) return;
+
+            Debug.Log("UNITY DEBUG LOG:\n A user has EXITED your proximity: " + playerID);
+            if (playerID != null)
+            {
+                Agora.Unsubscribe(playerID);
+            }
+        }
+    }
+
+
+}
 
 
