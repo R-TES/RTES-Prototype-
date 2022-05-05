e3a0e3b73669c040885e2dfe64a43976cc60733e
Corruption13 <sandeey999@gmail.com>
2022-05-06 04:39:37 +0530

    small stuff

--- Assets/Assets/Scripts/Interactable Level Objects/doorTeleportToRoomScript.cs
+++ Assets/Assets/Scripts/Interactable Level Objects/doorTeleportToRoomScript.cs
@@ -10,7 +10,8 @@ public class doorTeleportToRoomScript : MonoBehaviour
     // Start is called before the first frame update
     void OnTriggerEnter2D(Collider2D col)
     {
-        
+        if (col.gameObject.CompareTag("IgnoreTriggers")) return; // Don't teleport objects with this tag.
+
         if(!doorController.checkIfAlreadyBlocked(col.gameObject)){
             col.gameObject.transform.position = destination.transform.position; 
             doorController.addToBlockList(col.gameObject);
@@ -18,6 +19,7 @@ public class doorTeleportToRoomScript : MonoBehaviour
     }
 
     void OnTriggerExit2D(Collider2D col){
+        if(col.gameObject.CompareTag("IgnoreTriggers")) return;
         doorController.removeFromBlockList(col.gameObject);
     }
 }
--- testingBuild/Build/testingBuild.framework.js
+++ testingBuild/Build/testingBuild.framework.js
@@ -1166,26 +1166,26 @@ var tempDouble;
 var tempI64;
 
 var ASM_CONSTS = {
- 3876692: function() {
+ 3876820: function() {
   Module["emscripten_get_now_backup"] = performance.now;
  },
- 3876747: function($0) {
+ 3876875: function($0) {
   performance.now = function() {
    return $0;
   };
  },
- 3876795: function($0) {
+ 3876923: function($0) {
   performance.now = function() {
    return $0;
   };
  },
- 3876843: function() {
+ 3876971: function() {
   performance.now = Module["emscripten_get_now_backup"];
  },
- 3876898: function() {
+ 3877026: function() {
   return Module.webglContextAttributes.premultipliedAlpha;
  },
- 3876959: function() {
+ 3877087: function() {
   return Module.webglContextAttributes.preserveDrawingBuffer;
  }
 };
