70b0cef35193ca5f86cf6e383984993d0d321228
Corruption13 <sandeey999@gmail.com>
2022-05-19 20:53:39 +0530

    build

--- Assets/Assets/Scripts/Photon/SelfDestructObjectOnPlayerDisconnect.cs
+++ Assets/Assets/Scripts/Photon/SelfDestructObjectOnPlayerDisconnect.cs
@@ -8,18 +8,11 @@ public class SelfDestructObjectOnPlayerDisconnect : MonoBehaviourPunCallbacks
 {
     // Start is called before the first frame update
     PhotonView targetObjectPV;
-
-    private void Start()
-    {
-        targetObjectPV = GetComponent<PhotonView>();
-    }
-
-    public override void OnPlayerLeftRoom(Photon.Realtime.Player otherplayer)
+    public override void OnPlayerLeftRoom(Player otherplayer)
     {
         if (targetObjectPV.Owner.NickName != otherplayer.NickName) return;    // Ignore if gameobject isn't owned by player who disconnected.
         if (targetObjectPV.gameObject != null)
         {
-            
             PhotonNetwork.Destroy(targetObjectPV.gameObject);  // Claim ownership and destroy gameobject otherwise.
         }
     }
